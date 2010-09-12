fd324ddeec8d80a934f74b2a7c7df993b0ccf96d
Colin Seymour <lildood@gmail.com>
2010-09-12 10:47:05 +0100

    Updated examples
    --HG--
    rename : examples/example-recent.php => examples/example-popular.php

--- examples/example-popular.php
+++ examples/example-popular.php
@@ -1,12 +1,12 @@
 <html>
 <head>
-<title>phpZenfolio Recent Galleries Example</title>
+<title>phpZenfolio Popular Galleries Example</title>
 </head>
 <body>
 <?php
 /* Last updated with phpZenfolio 1.0
  *
- * This example file shows you how to get a list of the most recent public
+ * This example file shows you how to get a list of the most popular public
  * galleries created on Zenfolio.
  *
  * You'll need to replace:
@@ -22,7 +22,7 @@ require_once("../phpZenfolio.php");
 try {
 	$f = new phpZenfolio("AppName=<APP NAME/VER (URL)>");
 	// Get list of recent galleries
-	$galleries = $f->GetRecentSets('Gallery', 0, 100);
+	$galleries = $f->GetPopularSets('Gallery', 0, 100);
 	// Display the 60x60 cropped thumbnails and link to the gallery page for each.
 	foreach ($galleries as $gallery) {
 		echo '<a href="',$gallery['PageUrl'],'"><img src="',phpZenfolio::imageUrl($gallery['TitlePhoto'], 1),'" title="',$gallery['Title'],'" alt="',$gallery['Id'],'" /></a>';
--- examples/example-user.php
+++ examples/example-user.php
@@ -1,16 +1,18 @@
 <html>
 <head>
-<title>phpZenfolio User Galleries Example</title>
+<title>phpZenfolio First User Gallery Example</title>
 </head>
 <body>
 <?php
 /* Last updated with phpZenfolio 1.0
  *
  * This example file shows you how to get a list of the specified user's public
- * galleries created on Zenfolio.
+ * galleries and collections created on Zenfolio and display the first 100 images
+ * in the first gallery or collection
  *
  * You'll need to replace:
  * - <APP NAME/VER (URL)> with your application name, version and URL
+ * - <USERNAME> with the Zenfolio username you wish to view
  *
  * The application name and version is required, but there is no required format.
  * See the README.txt for a suggested format.
@@ -21,11 +23,13 @@ require_once("../phpZenfolio.php");
 
 try {
 	$f = new phpZenfolio("AppName=<APP NAME/VER (URL)>");
-	// Get list of recent galleries
-	$galleries = $f->GetRecentSets('Gallery', 0, 100);
-	// Display the 60x60 cropped thumbnails and link to the gallery page for each.
-	foreach ($galleries as $gallery) {
-		echo '<a href="',$gallery['PageUrl'],'"><img src="',phpZenfolio::imageUrl($gallery['TitlePhoto'], 1),'" title="',$gallery['Title'],'" alt="',$gallery['Id'],'" /></a>';
+	// Get list of recent galleries and collections
+	$h = $f->LoadGroupHierarchy("<USERNAME>");
+	// Get all the pictures in the first element
+	$pictures = $f->LoadPhotoSetPhotos($h['Elements'][0]['Id'], 0, 100 );
+	// Display the 60x60 cropped thumbnails and link to the photo page for each.
+	foreach ($pictures as $pic) {
+		echo '<a href="',$pic['PageUrl'],'"><img src="',phpZenfolio::imageUrl($pic, 1),'" title="',$pic['Title'],'" alt="',$pic['Id'],'" /></a>';
 	}
 }
 catch (Exception $e) {
