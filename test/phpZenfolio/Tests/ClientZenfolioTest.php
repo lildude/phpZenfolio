362cf87d9a25b18725d5088c15a837c18e548da5
Colin Seymour <lildood@gmail.com>
2016-06-20 01:37:50 +0100

    Start to implement Zenfolio interaction tests

---
+++ test/phpZenfolio/Tests/ClientZenfolioTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace phpZenfolio\Tests;
+
+use phpZenfolio\Client;
+use GuzzleHttp\Client as GuzzleClient;
+
+class ClientZenfolioTest extends \PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+        $this->client = new \phpSmug\Client('phpZenfolio Unit Testing/'.phpZenfolio\Client::VERSION.' (https://phpzenfolio.com)');
+        $this->client->setAuthToken(getenv('AUTH_TOKEN'));
+    }
+
+    /**
+     * A quick handy feature to ensure we don't attempt to run any tests if these
+     * env vars aren't set.
+     */
+    public function checkEnvVars()
+    {
+        if (empty(getenv('AUTH_SECRET'))) {
+            $this->markTestSkipped("Environment variable $env_var not set.");
+
+            return;
+        }
+    }
+
+}
